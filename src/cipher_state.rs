55361c0a204a7ee41f17e310f9aa634d72c6371d
Ben PHL <benphawke@gmail.com>
2024-07-08 17:20:20 +0200

    Iterate on decryption

--- src/cipher_state.rs
+++ src/cipher_state.rs
@@ -85,18 +85,11 @@ impl CipherState {
             !self.key.0.iter().any(|&b| b != 0),
             "invariant broken: attempt to decrypt wth empty key"
         );
-        let Ok((text, me_res)) = decrypt(&self.key, self.nonce, assosciated_data, cipher_text)
-        else {
+        let Ok(res) = self.do_decrypt(assosciated_data, cipher_text) else {
             panic!("todo: recover the previous nonce, and return error");
         };
 
-        Ok((
-            me_res.map(|n| Self {
-                nonce: n,
-                key: self.key,
-            }),
-            text,
-        ))
+        Ok(res)
     }
 
     /// Trades a nonce/key pair and plain-text +ad pair for ciphertext + next nonce
@@ -124,24 +117,24 @@ impl CipherState {
         };
         (Some(fresh), ct)
     }
-}
 
-// attempt to trade nonce + ciphertext for plaintext
-fn decrypt(
-    key: &CipherKey,
-    nonce: Nonce,
-    ad: &[u8],
-    mut text: CipherText,
-) -> Result<(PlainText, Option<Nonce>), (CipherText, Nonce, CipherError)> {
-    assert!(
-        !key.0.iter().any(|&b| b != 0),
-        "invariant broken: attempt to decrypt wth empty key"
-    );
+    /// Attempt to trade key + nonce pair to decrypt an ad + ct pair for plain-text and next-key +
+    /// nonce par
+    pub(crate) fn do_decrypt(self, ad: &[u8], mut text: CipherText) -> Result<(Option<Self>, PlainText), (CipherText, Nonce, CipherError)> {
+        assert!(
+            self.key.valid_key(),
+            "invariant broken: attempt to decrypt wth empty key"
+        );
 
-    let aead = chacha20poly1305::ChaCha20Poly1305::new(&key.0.into());
-    let (n_arr, next_nonce) = nonce.chacha_harvest();
-    match aead.decrypt_in_place_detached(&n_arr.into(), ad, &mut text.text, &text.tag) {
-        Ok(()) => Ok((PlainText(text.text), next_nonce)),
-        Err(_) => todo!("auth error: recover previous nonce and return it and ciphertext"),
+        let aead = chacha20poly1305::ChaCha20Poly1305::new(&self.key.0.into());
+        let (n_arr, next_nonce) = self.nonce.chacha_harvest();
+        let decrypt =  aead.decrypt_in_place_detached(&n_arr.into(), ad, &mut text.text, &text.tag);
+
+        let Ok(()) = decrypt else {
+            todo!("auth error: recover previous nonce and return it and ciphertext");
+        };
+
+        let res = next_nonce.map(|nn| Self{ nonce: nn, key: self.key });
+        Ok((res, PlainText(text.text)))
     }
 }
