867b1b95cc8f3ce49ba635de94dfbbd6311d59ed
Ben <benphawke@gmail.com>
2025-04-03 13:44:15 +0200

    Add flake.nix (#2)

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Base flake for rust project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            rust-bin.stable.latest.default
+          ];
+
+          shellHook = ''
+            echo "in rust dev shell";
+          '';
+        };
+      }
+    );
+}
+
